033046690743bfd063f5b8d2af38794f52aaca78
Daniel Yuste <123724286+Daniyus05@users.noreply.github.com>
2023-04-14 17:04:17 +0200

    modificación par o impar

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("¡Hola! a continuación le voy a pedir que me escriba dos números,seguidamente le indicaré si el segundo es par o impar. ");
+Console.WriteLine("¡Hola! a continuación le voy a pedir que escriba dos números, seguidamente le indicaré cual es mayor o menor, o si son iguales y si el segundo es par o impar. ");
 Console.WriteLine("¿Cuál es el primer número que has elegido?");
 var primerNumero = Console.ReadLine();
 int numero1 = int.Parse(primerNumero);
@@ -8,21 +8,21 @@ var segundoNumero = Console.ReadLine();
 int numero2 = int.Parse(segundoNumero);
 if (numero1 > numero2)
 {
-    Console.WriteLine("El primer número es mayor que el segundo número");
+    Console.WriteLine("El " + numero1 + " es mayor a " + numero2);
 }
 else if (numero1 < numero2)
 {
-    Console.WriteLine("El primer número es menor que el segundo número.");
+    Console.WriteLine("El " + numero1 + " es menor a " + numero2);
 }
 else
 {
-    Console.WriteLine("El primer número es igual al segundo número.");
+    Console.WriteLine("El " + numero1 + " es igual a " + numero2);
 }
 if (numero2 % 2 == 0)
 {
-    Console.WriteLine("El segundo número es par");
+    Console.WriteLine("El " + numero2 + " es par");
 }
 else
 {
-    Console.WriteLine("El segundo número es impar");
+    Console.WriteLine("El " + numero2 + " es impar");
 }
